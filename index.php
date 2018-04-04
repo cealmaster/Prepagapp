84ee3bab4db5827c1bb5cc7974dbe317860f7a7c
felipe813 <felipe_813@hotmail.com>
2018-04-04 18:28:16 -0500

    Pruebita2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<meta content="Prepagos a domicilio" name="subject" >
     <meta content="http://www.prepagapp.com" name="url">
     <meta content="Las mejores prepagos y putas en Bogota a domicilio, dispuestas a satisfacer tus deseos mas calientes." name="description">
-    <title>Prepagapp</title>
+    <title>Prepagapp--prueba</title>
     <?php include "./partes/head.php"; ?>
 </head>
 
